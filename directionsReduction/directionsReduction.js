ace5fa17ca81221bc1e2167508412ee30100050b
ENGMENJARI <anjari1981@gmail.com>
2017-01-30 08:33:20 +0200

    ww

--- directionsReduction/directionsReduction.js
+++ directionsReduction/directionsReduction.js
@@ -27,61 +27,89 @@ dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) => ["WEST
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]) => [] //don't need to move at all
 */
 
-var dirReduc = function(directions){
-	var res=[]
-	for(var i=0;i<directions.length;i++){
-		if(res.indexOf(directions[i])=== -1){
-			res.push(directions[i])
-		}
-	}
+// var dirReduc = function(directions){
+//      var sum=0
+// 	for(var i=0;i<directions.length;i++){
+  
+// 		if(directions[i]==='SOUTH'){
+// 			directions[i]=1
+// 		}
+// 		if(directions[i]==='NORTH'){
+// 			directions[i]=-1
+// 		}
+// 		if(directions[i]==='WEST'){
+// 			directions[i]=2
+// 		}
+// 		if(directions[i]==='EAST'){
+// 			directions[i]=-2
+// 		}
+//      sum+=directions[i]
+//  }
+//      if(sum===0){
+//      	return 'No need to move at all'
+//      }else if(sum===2){
+//      	return 'move to WEST'
+//      }else if(sum===-2){
+//      	return 'move to EAST'
+//      }else if (sum===1){
+//      	return 'move to SOUTH'
+//      }else if(sum===-1){
+//      	return 'move to NORTH'
+//      }else if(sum===3){
+//      	if(directions[0]===1){
+//      		return 'go to SOUTH then WEST'
+//      	}return 'go to WEST then SOUTH'
+//      }else if(sum===-1){
+//      	if(directions[0]===1){
+//      		return 'go to SOUTH then EAST'
+//      	}return 'go to EAST then SOUTH'
+//      }else if(sum===1){
+//      	if(directions[0]===-1){
+//      		return 'go to NORTH then WEST'
+//      	}return 'go to WEST then NORTH'
+//      }else{
+//      	// if(sum===-3){
+//      		if(directions[0]===-1){
+//      			return 'go to NORTH then EAST'
+//      		}
+//                return 'go to EAST then NORTH'
+//      	}
+     
 	
-  var sum=0
-	for(var j=0;j<res.length;j++){
-		if(res[j]==='SOUTH'){
-			res[j]=1
-		}
-		if(res[j]==='NORTH'){
-			res[j]=-1
-		}
-		if(res[j]==='WEST'){
-			res[j]=2
-		}
-		if(res[j]==='EAST'){
-			res[j]=-2
-		}
-     sum+=res[j]
- }
-     if(sum===0){
-     	return 'No need to move at all'
-     }else if(sum===2){
-     	return 'move to WEST'
-     }else if(sum===-2){
-     	return 'move to EAST'
-     }else if (sum===1){
-     	return 'move to SOUTH'
-     }else if(sum===-1){
-     	return 'move to NORTH'
-     }else if(sum===3){
-     	if(res[0]===1){
-     		return 'go to SOUTH then WEST'
-     	}return 'go to WEST then SOUTH'
-     }else if(sum===-1){
-     	if(res[0]===1){
-     		return 'go to SOUTH then EAST'
-     	}return 'go to EAST then SOUTH'
-     }else if(sum===1){
-     	if(res[0]===-1){
-     		return 'go to NORTH then WEST'
-     	}return 'go to WEST then NORTH'
-     }else{
-     	if(sum===-3){
-     		if(res[0]===-1){
-     			return 'go to NORTH then EAST'
-     		}return 'go to EAST then NORTH'
-     	}
-     }
-	
-	}
+// 	}
 
+
+var dirReduc = function(directions){
+     var No=0
+     var Ea=0
+     var arr=[];
+     for (var i = 0; i < directions.length; i++) {
+          if (directions[i] ==='NORTH'){
+               No++;
+          }else if (directions[i]==='SOUTH') {
+               No--;
+          }else if (directions[i]==='EAST') {
+               Ea++;
+          }else if (directions[i]==='WEST') {
+               Ea--;
+          }
+     }
+     for (var i = 0; i < Math.abs(No)+Math.abs(Ea); i++) {
+          if (No>0){
+               arr.push('NORTH');
+               No--;
+          }else if (No<0){
+               arr.push('SOUTH');
+               No++;
+          }if (Ea>0){
+               arr.push('EAST');
+               Ea--;
+          }else if (Ea<0){
+               arr.push('WEST');
+               Ea++;
+          }
+     }
+     return arr;
+};
 
 
